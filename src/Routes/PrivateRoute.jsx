bd3f66a5cfc0e1bf678b78ea54d43bb38f38e551
Alamin0x01 <md.alamin.ali.096@gmail.com>
2023-06-05 00:55:54 +0600

    Google Auth Signin mathod added

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,17 +1,17 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../Provider/AuthProvider';
-import { Navigate, useLocation } from 'react-router-dom';
+import React, { useContext } from "react";
+import { AuthContext } from "../Provider/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-    const { user, loading } = useContext(AuthContext);
-    let location = useLocation();
-    if (loading) {
-        return <progress className="progress w-56"></progress>
-    }
-    if (user) {
-        return children;
-    }
-    return <Navigate state={{ from: location }} to='/signin' replace></Navigate>
+  const { user, loading } = useContext(AuthContext);
+  let location = useLocation();
+  if (loading) {
+    return <progress className="progress w-56"></progress>;
+  }
+  if (user) {
+    return children;
+  }
+  return <Navigate state={{ from: location }} to="/signin" replace></Navigate>;
 };
 
 export default PrivateRoute;
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import routes from './Routes/Routes.jsx'
-import AuthProvider from './Provider/AuthProvider'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./Routes/Routes.jsx";
+import AuthProvider from "./Provider/AuthProvider";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthProvider>
       <RouterProvider router={routes}></RouterProvider>
     </AuthProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
